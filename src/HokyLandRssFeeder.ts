3537bb0c14be80b08266ea9faac39d6993fe4821
Whimsicott <eruhoon@gmail.com>
2020-09-06 12:36:25 +0900

    add HokyPublisher

--- src/HokyLandRssFeeder.ts
+++ src/HokyLandRssFeeder.ts
@@ -1,16 +1,9 @@
-import { HokyFeedMaker } from './models/feed/HokyFeedMaker';
-import { HokyItemParserImpl } from './models/parse/HokyItemParserImpl';
-import { SimpleHttpFetcher } from './models/fetch/SimpleUrlFetcher';
 import express from 'express';
+import { HokyPublisher } from './models/publish/HokyPublisher';
 
 export class HokyLandRssFeeder {
   public async main(): Promise<void> {
-    const url =
-      'http://hoky.co.kr/template/1/categ_list.asp?categ_code=97&page=1&cv=6&orderOPT=0&pagesize=100';
-    const contents = await new SimpleHttpFetcher().fetch(url);
-    const items = new HokyItemParserImpl().parse(contents);
-    const feed = new HokyFeedMaker().makeFeed(items);
-
+    const feed = await new HokyPublisher().publish();
     const app = express();
     const port = 3000;
 
